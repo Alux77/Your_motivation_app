1a3b70282ad4a932167afecd0115f5593e40d1ee
alux77 <alux.cervantes@gmail.com>
2017-05-10 14:34:05 -0500

    Add Soon message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: 'Sooner: our first motivation site: Just for you'
+  end
+
 end
